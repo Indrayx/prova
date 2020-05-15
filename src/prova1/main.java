b92e5efef93c3a05d47fb319ff449ffef158f111
Lucas Augusto <lucasamgs@gmail.com>
2020-05-15 20:45:30 -0300

    Cabeçalho

--- src/prova1/main.java
+++ src/prova1/main.java
@@ -1,9 +1,45 @@
 package prova1;
-
+import java.util.Locale;
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+ 
+		
+		int compras,atraso,pay,scoreatraso,scorecompras;
+		double ticket,totalcompras, scoretotal;
+		char pagamento;
+		pay = 0;
+		scoreatraso = 0;
+		scorecompras = 0;
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("-----------------------------");
+		System.out.println("INFORME OS DADOS DO ULTIMO ANO");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
